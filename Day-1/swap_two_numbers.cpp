7363b5e75d228019d12a8dce9746536bdad61c6c
Tanishka <tanishkabansalksg@gmail.com>
2026-06-19 23:00:19 +0530

    swap two numbers

---
+++ Day-1/swap_two_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping:\n";
+    cout << "First number = " << a << endl;
+    cout << "Second number = " << b << endl;
+
+    return 0;
+}
