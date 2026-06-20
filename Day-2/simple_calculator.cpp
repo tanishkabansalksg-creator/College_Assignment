ceac1085b56849bc07699e326949502dccaccfad
Tanishka <tanishkabansalksg@gmail.com>
2026-06-20 11:40:07 +0530

    simple calculator

---
+++ Day-2/simple_calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+
+    cin >> a >> op >> b;
+
+    switch(op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        default: cout << "Invalid";
+    }
+
+    return 0;
+}
