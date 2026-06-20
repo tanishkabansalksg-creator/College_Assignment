a4e46b6f61c3de251950ce9608a28fa73cc2a7ff
Tanishka <tanishkabansalksg@gmail.com>
2026-06-20 11:35:56 +0530

    positve negative zero

---
+++ Day-2/positive_negative_zero.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+        printf("Positive Number");
+    else if (num < 0)
+        printf("Negative Number");
+    else
+        printf("Zero");
+
+    return 0;
+}
