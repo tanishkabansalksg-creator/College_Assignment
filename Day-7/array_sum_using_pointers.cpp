d590c1c9c81b058c4ca7bb7c5020e5c959538a08
Tanishka <tanishkabansalksg@gmail.com>
2026-06-23 16:51:42 +0530

    array

---
+++ Day-7/array_sum_using_pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *p = arr;
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += *(p + i);
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
