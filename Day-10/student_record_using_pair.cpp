df9850748cd2410a953c30c65cd80f045ba468b3
Tanishka <tanishkabansalksg@gmail.com>
2026-06-25 23:35:46 +0530

    student

---
+++ Day-10/student_record_using_pair.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main()
+{
+pair<int,string> student = {123,"xyz"};
+cout<<student.first<<endl;
+cout<<student.second;
+return 0;
+}
