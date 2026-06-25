6327d70a11c8350dd33ded8d855477f0cf40e84e
Tanishka <tanishkabansalksg@gmail.com>
2026-06-25 23:32:43 +0530

    rotate

---
+++ Day-9/rotate_vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+vector<int> v = {1,2,3,4,5};
+rotate(v.begin(),
+v.begin()+3,
+v.end());
+for(int x : v)
+cout<<x<<" ";
+return 0;
