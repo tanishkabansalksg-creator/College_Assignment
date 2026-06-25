e9852e7e7f8f8f88dd76210ce5370e549ee2d54c
Tanishka <tanishkabansalksg@gmail.com>
2026-06-25 23:29:11 +0530

    everse

---
+++ Day-9/reverse_vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+vector<int> v = {1,2,3,4,5};
+reverse(v.begin(), v.end());
+for(int x : v)
+cout<<x<<" ";
+return 0;
+}
