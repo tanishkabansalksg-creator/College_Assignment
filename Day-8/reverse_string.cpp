db9c5f2afbf6cf1fdd64fff25c01259d48752d62
Tanishka <tanishkabansalksg@gmail.com>
2026-06-25 23:13:32 +0530

    reverse

---
+++ Day-8/reverse_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void reverse(string s) {
+    if (s.length() == 0)
+        return;
+    reverse(s.substr(1));
+    cout << s[0];
+}
+
+int main() {
+    string s;
+    cin >> s;
+    reverse(s);
+}
